c54fee20f5de9e4e9af6d1d7697aae10a36dc0e9
waverew <waverew@gmail.com>
2023-03-27 18:08:42 -0400

    added routing

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
--- landing/src/components/Login.js
+++ landing/src/components/Login.js
@@ -9,35 +9,39 @@ const Login = () => {
                 <h1>Let's <label>Vote!</label></h1>
             </div>
             <div className='form'>
-            <label><b>Welcome back</b>
-            <br></br>Login to your account</label>
-            
-            <form onSubmit={console.log('bebra')}>
-                <p>Username</p>
-                <label>
-                    <img src="https://cdn-icons.flaticon.com/svg/3917/3917688.svg?token=exp=1679712291~hmac=a5d57eb496158ad3666c3d6fa08bc569" alt="" className="img">
-                    </img>
-                </label>
-                <input type="text" defaultValue={""} name="login">
-                </input>
-                <br></br>
-                
-                <p>Password</p>
-                <label>
-                    <img className='img' src="https://cdn-icons.flaticon.com/svg/3917/3917591.svg?token=exp=1679712291~hmac=8c3bbf022038e6389385b937a771d444" alt="">
-                    </img>
-                </label>
-                <input type="password" defaultValue={""} name="pass">
-                </input>
-                <br></br>
-                <div className='inp-cont'>
-                    <div>
-                    <input type='checkbox'></input>
-                    <label>Remember me</label>
+                <div className='form-cont'>
+                    <div className='greet-div'>
+                        <b>Welcome back</b>
+                        <br></br>
+                        <p>Login to your account</p>
                     </div>
-                    <button type="submit" className='but'>Login</button>
+                <form onSubmit={console.log('bebra')}>
+                    <p>Username</p>
+                    <label>
+                        <img src="https://cdn-icons.flaticon.com/svg/3917/3917688.svg?token=exp=1679712291~hmac=a5d57eb496158ad3666c3d6fa08bc569" alt="" className="img">
+                        </img>
+                    </label>
+                    <input type="text" defaultValue={""} name="login">
+                    </input>
+                    <br></br>
+                    
+                    <p>Password</p>
+                    <label>
+                        <img className='img' src="https://cdn-icons.flaticon.com/svg/3917/3917591.svg?token=exp=1679712291~hmac=8c3bbf022038e6389385b937a771d444" alt="">
+                        </img>
+                    </label>
+                    <input type="password" defaultValue={""} name="pass">
+                    </input>
+                    <br></br>
+                    <div className='inp-cont'>
+                        <div>
+                        <input type='checkbox'></input>
+                        <label>Remember me</label>
+                        </div>
+                        <button type="submit" className='but'>Login</button>
+                    </div>
+                </form>
                 </div>
-            </form>
             </div>
         </div>
     )
