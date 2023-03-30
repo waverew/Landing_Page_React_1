546c96fbbd6a76a131fd4b14f7b050cc1658c5be
waverew <waverew@gmail.com>
2023-03-29 20:40:51 -0400

    Started working on dashboard

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import Login from './components/Login';
+import "./App.css";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
---
+++ landing/src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import Logo from "./Logo";
+import "./Dashboard.css";
+const Dashboard = () => {
+    return (
+        <div className="Dashboard">
+            <Logo></Logo>
+        </div>
+    );
+}
+export default Dashboard;
