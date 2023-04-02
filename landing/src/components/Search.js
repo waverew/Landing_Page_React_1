5c225b4c3a7669011d46146c922cc504b35968ea
waverew <waverew@gmail.com>
2023-04-01 20:54:15 -0400

    same

--- landing/src/components/Search.js
+++
@@ -1,8 +0,0 @@
-const Search = () => {
-    return (
-        <div className="Search">
-            <input type="search" name="search"></input>
-        </div>
-    );
-}
-export default Search;
---
+++ landing/src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import "./SearchBar.css";
+const SearchBar = () => {
+  return (
+    <div className="SearchBar">
+      <input type="search" name="searchBar"></input>
+    </div>
+  );
+};
+export default SearchBar;
