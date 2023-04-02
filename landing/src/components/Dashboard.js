bc6bf9afeb1c51ea1c82c0b096e6bdc8645398c6
waverew <waverew@gmail.com>
2023-04-01 21:14:06 -0400

    added searchbar css

--- landing/src/components/Dashboard.js
+++ landing/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import Logo from "./Logo";
 import "./Dashboard.css";
+import SearchBar from "./SearchBar";
 import LeftMenu from "./LeftMenu";
 const Dashboard = () => {
   return (
     <div className="Dashboard">
       <Logo/>
+      <SearchBar/>
       <div className="menu">
         <LeftMenu />
       </div>
