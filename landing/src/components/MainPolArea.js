8500d5ec9f0b03e3e7c47767bc87b112e5fc2dd9
waverew <waverew@gmail.com>
2023-04-04 17:38:29 -0400

    added recent polling components

--- landing/src/components/MainPolArea.js
+++ landing/src/components/MainPolArea.js
@@ -1,10 +1,18 @@
 import "./MainPolArea.css";
 import FinalPolCompact from "./FinalPolCompact";
+import RecentPol from "./RecentPol";
 const MainPolArea = () => {
   return (
     <div className="MainPolArea">
-      <div>
+      <h2>Final Pol</h2>
+      <div className="final-pol">
         <FinalPolCompact name="Marco" />
+        <FinalPolCompact name="Stacy" />
+      </div>
+      <h2 className="ab">Recent Polling</h2>
+      <div className="recent-pol">
+        <RecentPol name="Barbra" />
+        <RecentPol name="Megan" />
       </div>
     </div>
   );
---
+++ landing/src/components/RecentPol.js
@@ -0,0 +1,15 @@
+import "./RecentPol.css";
+const RecentPol = (props) => {
+  return (
+    <div className="RecentPol">
+      <img src="./img/dashboard/polpic.png" alt="" className="polpic"></img>
+      <div>
+        <p>
+          <b>{props.name}</b>
+        </p>
+        <p>Selection</p>
+      </div>
+    </div>
+  );
+};
+export default RecentPol;
