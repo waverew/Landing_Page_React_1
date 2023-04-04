95c623468c3442f65ea8c386f4538c7e4f0befe1
waverew <waverew@gmail.com>
2023-04-03 23:28:30 -0400

    adding polling comp

---
+++ landing/src/components/FinalPolCompact.js
@@ -0,0 +1,12 @@
+import './FinalPolCompact.css';
+const FinalPolCompact = (props) => {
+  return (
+    <div className="FinalPolCompact">
+      <p>
+        <b>{props.name}</b>
+      </p>
+      <p>Selection</p>
+    </div>
+  );
+};
+export default FinalPolCompact;
